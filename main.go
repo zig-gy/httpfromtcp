0317ea500470ac0dcb6c25bc2fdf40080a102c96
zig-gy <benjamin.f.chadwick@proton.me>
2025-08-13 12:18:54 -0400

    implemented file reading

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Printf("error opening file: %v\n", err)
+		os.Exit(1)
+		return
+	}
+
+	for {
+		fileBytes := make([]byte, 8)
+		_, err = file.Read(fileBytes)
+
+		if err == io.EOF {
+			fmt.Println("file finished")
+			os.Exit(0)
+			return
+		}
+
+		if err != nil {
+			fmt.Printf("error reading file: %v\n", err)
+			os.Exit(1)
+			return
+		}
+
+		fmt.Printf("read: %s\n", fileBytes)
+	}
 }
