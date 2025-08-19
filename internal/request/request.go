bf76233e0b3448709f6f67ee044f4765f09c67ce
zig-gy <benjamin.f.chadwick@proton.me>
2025-08-19 15:25:21 -0400

    added request parsing functionality and tests

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,62 @@
 package request
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	req, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, fmt.Errorf("could not read from reader: %v", err)
+	}
+
+	requestLine, err := parseRequestLine(string(req))
+	if err != nil {
+		return nil, fmt.Errorf("could not parse request line: %v", err)
+	}
+
+	for _, letter := range requestLine.Method {
+		if !unicode.IsUpper(letter) {
+			return nil, fmt.Errorf("method line malformed, not all uppercase alphabetic")
+		}
+	}
+
+	if !strings.Contains(requestLine.HttpVersion, "1.1") {
+		return nil, fmt.Errorf("http version does not match")
+	}
+
+	request := Request{RequestLine: requestLine}
+	return &request, nil
+}
+
+func parseRequestLine(request string) (RequestLine, error) {
+	reqParts := strings.Split(request, "\r\n")
+	lineParts := strings.Split(reqParts[0], " ")
+	if len(lineParts) < 3 {
+		return RequestLine{}, fmt.Errorf("request malformed, not enough fields")
+	}
+
+	httpVersionParts := strings.Split(lineParts[2], "/")
+	if httpVersionParts[0] != "HTTP" {
+		return RequestLine{}, fmt.Errorf("not an http request")
+	}
+
+	return RequestLine{
+		HttpVersion:   httpVersionParts[1],
+		RequestTarget: lineParts[1],
+		Method:        lineParts[0],
+	}, nil
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,47 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLine(t *testing.T) {
-	assert.Equal(t, "Testing", "Testing")
+	// Test: Good GET Request line
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good GET Request line with path
+	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Invalid number of parts in request line
+	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Good POST Request line with path
+	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Invalid method
+	_, err = RequestFromReader(strings.NewReader("get /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid http version
+	_, err = RequestFromReader(strings.NewReader("get /coffee HTTP/2.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
 }
